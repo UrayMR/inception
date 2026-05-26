a8a0fdf92ccfe5047fc9342e8311e4353e63e3e3
UrayMR <umrajsyafiq@gmail.com>
2026-05-26 15:25:12 +0700

    refactor: adding prefix route for better route organizations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Guest\CompetitionRegistrationController;
+use App\Http\Controllers\Participant\CompetitionRegistrationController;
 use App\Http\Controllers\Panel\CompetitionController;
 use App\Http\Controllers\Panel\TeamController;
 use App\Http\Controllers\Panel\TransactionController;
@@ -9,30 +9,37 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
 Route::inertia('/', 'guest/main', [
-    // 'canRegister' => Features::enabled(Features::registration()),
+    'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::middleware('role:admin')->group(function () {
-        Route::inertia('dashboard', 'panel/dashboard')->name('dashboard');
-        Route::resource('users', UserController::class)->names('users');
-        Route::resource('competitions', CompetitionController::class)->names('competitions');
-        Route::resource('teams', TeamController::class)->names('teams');
-    });
+    Route::prefix('panel')->as('panel.')->group(function () {
+        Route::middleware('role:admin')->group(function () {
+            Route::inertia('dashboard', 'panel/dashboard')->name('dashboard');
+            Route::resource('users', UserController::class)->names('users');
+            Route::resource('competitions', CompetitionController::class)->names('competitions');
+            Route::resource('teams', TeamController::class)->names('teams');
+        });
 
-    Route::middleware('role:admin,accountant')->group(function () {
-        Route::controller(TransactionController::class)->group(function () {
-            Route::patch('transactions/verify/{transaction}', 'verify')->name('transactions.verify');
-            Route::patch('transactions/reject/{transaction}', 'reject')->name('transactions.reject');
-            Route::resource('transactions', TransactionController::class)->names('transactions');
+        Route::middleware('role:admin,accountant')->group(function () {
+            Route::controller(TransactionController::class)->group(function () {
+                Route::patch('transactions/verify/{transaction}', 'verify')->name('transactions.verify');
+                Route::patch('transactions/reject/{transaction}', 'reject')->name('transactions.reject');
+                Route::resource('transactions', TransactionController::class)->names('transactions');
+            });
         });
     });
 
-    Route::controller(CompetitionRegistrationController::class)->group(function () {
-        Route::get('competitions', 'index')->name('competitions.index');
-        Route::get('competitions/{competition}', 'show')->name('competitions.show');
-        Route::get('competitions/register', 'register')->name('competitions.register');
-        Route::post('competitions/register', 'store')->name('competitions.register.store');
+    Route::as('participant.')->group(function () {
+        Route::controller(CompetitionRegistrationController::class)->group(function () {
+            Route::get('competitions', 'index')->name('competitions.index');
+
+            Route::get('competitions/register', 'register')->name('competitions.register');
+            Route::post('competitions/register', 'store')->name('competitions.register.store');
+
+            // Put this route after the register route to avoid conflict with the 'register' URI.
+            Route::get('competitions/{competition}', 'show')->name('competitions.show');
+        });
     });
 });
 

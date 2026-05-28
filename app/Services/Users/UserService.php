abfc636f9df566db7c4681383b957e8e57583e88
UrayMR <umrajsyafiq@gmail.com>
2026-05-28 09:11:10 +0700

    refactor: implement user management actions with Store, Update, and Delete functionalities

--- app/Services/Users/UserService.php
+++ app/Services/Users/UserService.php
@@ -2,9 +2,6 @@
 
 namespace App\Services\Users;
 
-use App\DTOs\Users\StoreUserDTO;
-use App\DTOs\Users\UpdateUserDTO;
-use App\Models\User;
 use App\Repositories\Users\UserRepository;
 use Illuminate\Http\Request;
 
@@ -24,37 +21,5 @@ class UserService
         ];
 
         return $this->userRepository->index($queryParams);
-    }
-
-    public function create(StoreUserDTO $dto): User
-    {
-        $attributes = [
-            'name' => $dto->name,
-            'email' => $dto->email,
-            'password' => $dto->password,
-            'role' => $dto->role->value,
-        ];
-
-        return $this->userRepository->store($attributes);
-    }
-
-    public function update(UpdateUserDTO $dto, User $user): User
-    {
-        $attributes = [
-            'name' => $dto->name,
-            'email' => $dto->email,
-            'role' => $dto->role->value,
-        ];
-
-        if ($dto->password) {
-            $attributes['password'] = $dto->password;
-        }
-
-        return $this->userRepository->update($attributes, $user);
-    }
-
-    public function destroy(User $user): bool
-    {
-        return $this->userRepository->destroy($user);
     }
 }

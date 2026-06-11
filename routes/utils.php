cc2537270a53db101e2804fb0b4746d5afdedab0
UrayMR <umrajsyafiq@gmail.com>
2026-06-11 13:30:43 +0700

    chore: add comment to clarify payment proof route in utils

--- routes/utils.php
+++ routes/utils.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\Utils\TransactionPaymentProofController;
 use Illuminate\Support\Facades\Route;
 
 Route::as('utils.')->middleware(['auth'])->group(function () {
+  
+  // Return the payment proof image for a transaction
   Route::get('/transactions/{transaction}/payment-proof', [TransactionPaymentProofController::class, 'show'])->name('transactions.payment-proof');
 });

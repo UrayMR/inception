44d0ab9cc20bc69e8c9e449709226b00fe52970c
UrayMR <umrajsyafiq@gmail.com>
2026-07-16 05:38:34 +0700

    feat: add submission routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Panel\AssignmentController;
 use App\Http\Controllers\Participant\CompetitionRegistrationController;
 use App\Http\Controllers\Panel\CompetitionController;
+use App\Http\Controllers\Panel\SubmissionController;
 use App\Http\Controllers\Panel\TeamController;
 use App\Http\Controllers\Panel\TransactionController;
 use App\Http\Controllers\Panel\UserController;
@@ -29,6 +30,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
             Route::resource('competitions', CompetitionController::class)->names('competitions');
             Route::resource('teams', TeamController::class)->names('teams');
             Route::resource('assignments', AssignmentController::class)->names('assignments');
+            Route::resource('submissions', SubmissionController::class)->names('submissions');
         });
 
         Route::middleware('role:admin,accountant')->group(function () {
